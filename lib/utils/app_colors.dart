6e8ac6b1909da6047052e21c5f706acd145ff1b7
mdtayobali13 <mdtayobalideveloper@gmail.com>
2025-05-28 16:38:10 +0600

    utils added

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFBAF15);
+  static const Color primaryShadow = Color(0xffE49F13);
+  static const Color secondaryColor = Color(0xff713694);
+  static const Color secondaryShadow = Color(0xff673187);
+  static const Color green = Color(0xffA5CD39);
+  static const Color red = Color(0xFFE32222);
+  static const Color grey = Color(0xFFC7C0BD);
+  static const Color greyShadow = Color(0xFF85766F);
+  static const Color chocolate = Color(0xFF4D3D36);
+  static const Color chocolateShadow = Color(0xFF200D04);
+  static const Color buttonGreen = Color(0xFFC3DE7A);
+  static const Color buttonGreenShadow = Color(0xFFA5CD39);
+}
---
+++ lib/zidney.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:zidney/utils/language/spunish.dart';
+
+class Zidney extends StatelessWidget {
+  const Zidney({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LanguageScreen(),
+    );
+  }
+}
