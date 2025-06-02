6cdaf85743cfa6d061c308b720d130a36cea958a
arafat593 <mdalarafat593@gmail.com>
2025-06-02 14:50:39 +0600

    update ans

--- lib/zidney.dart
+++ lib/zidney.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zidney/utils/themdata.dart';
 import 'package:zidney/view/auth/language_screen.dart';
+import 'package:zidney/view/screens/freePlanScreen/questionquiz/all_quiz.dart';
 import 'package:zidney/view/screens/freePlanScreen/questionquiz/many_part_qustion.dart';
 import 'package:zidney/view/screens/freePlanScreen/questionquiz/quiz.dart';
 
@@ -12,7 +13,7 @@ class Zidney extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Quiz(),
+      home: AllQuiz(),
       theme: themData(),
     );
   }
