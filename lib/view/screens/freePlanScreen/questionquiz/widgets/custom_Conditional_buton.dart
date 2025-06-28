ad927f5b6983183f472101be39733c43144fd122
arafat593 <mdalarafat593@gmail.com>
2025-06-28 11:40:36 +0600

    nothing changed

--- lib/view/screens/freePlanScreen/questionquiz/widgets/custom_Conditional_buton.dart
+++ lib/view/screens/freePlanScreen/questionquiz/widgets/custom_Conditional_buton.dart
@@ -37,32 +37,30 @@ class CustomConditionalButton extends StatelessWidget {
         child: Container(
           height: AppStyles.heightS,
           width: double.infinity,
-          decoration: isSelected
-              ? BoxDecoration(
-            color: backgroundColor,
-            borderRadius: borderRadius,
-            boxShadow: [
-              BoxShadow(
-                color: shadowColor,
-                offset: const Offset(0, 12),
-              ),
-            ],
-          )
-              : BoxDecoration(
-            color: Colors.white,
-            borderRadius: borderRadius,
-            border: Border.all(color: Colors.grey),
-          ),
+          decoration:
+              isSelected
+                  ? BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: borderRadius,
+                    boxShadow: [
+                      BoxShadow(
+                        color: shadowColor,
+                        offset: const Offset(0, 12),
+                      ),
+                    ],
+                  )
+                  : BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: borderRadius,
+                    border: Border.all(color: Colors.grey),
+                  ),
           child: Padding(
             padding: AppStyles.paddingHorizontalM,
             child: Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                if (prefix != null) ...[
-                  prefix!,
-                  const SizedBox(width: 10),
-                ],
+                if (prefix != null) ...[prefix!, const SizedBox(width: 10)],
                 Text(
                   buttonText,
                   style: TextStyle(
@@ -71,10 +69,7 @@ class CustomConditionalButton extends StatelessWidget {
                     color: isSelected ? Colors.white : textColor,
                   ),
                 ),
-                if (suffix != null) ...[
-                  const SizedBox(width: 10),
-                  suffix!,
-                ],
+                if (suffix != null) ...[const SizedBox(width: 10), suffix!],
               ],
             ),
           ),
